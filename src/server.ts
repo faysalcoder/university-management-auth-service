5cbd9a2fe7dcf441a594e02a9de089315f692dd4
faysalcoder <faysalsikder378486@gmail.com>
2023-05-30 14:22:55 +0600

    [Done] setuo express, mongoose, cors, parser

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 import app from "./app";
+import config from "./config";
 
 async function main() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect(config.database_url as string);
     console.log("🗄️__Database Connect Successfuly");
 
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
     });
   } catch (err) {
     console.log("Failed to connect database");
